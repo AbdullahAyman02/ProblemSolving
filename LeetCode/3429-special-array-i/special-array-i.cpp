1b52d79c6e770ef4232613a3bdd5bdd47176a14f
Abdullah Ayman <102327084+AbdullahAyman02@users.noreply.github.com>
2025-02-01 12:32:17 +0200

    Time: 0 ms (100.00%) | Memory: 27.6 MB (86.97%) - LeetSync

--- LeetCode/3429-special-array-i/special-array-i.cpp
+++ LeetCode/3429-special-array-i/special-array-i.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
+        if (nums.size() == 1)
+            return true;
         for (int i = 0, length = nums.size(); i < length - 1; i++) {
             if(nums[i] % 2 == nums[i + 1] % 2)
                 return false;
