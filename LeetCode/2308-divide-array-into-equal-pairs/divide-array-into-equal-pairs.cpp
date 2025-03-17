3355359468d5672f809d5234391473da2c3cb23a
Abdullah Ayman <102327084+AbdullahAyman02@users.noreply.github.com>
2025-03-17 20:51:40 +0200

    Time: 0 ms (100.00%) | Memory: 17.1 MB (64.71%) - LeetSync

--- LeetCode/2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ LeetCode/2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool divideArray(vector<int>& nums) {
+        if (nums.size() % 2 != 0)
+            return false;
         vector<int> freqArray(501, 0);
         for (auto& num : nums)
             freqArray[num]++;
