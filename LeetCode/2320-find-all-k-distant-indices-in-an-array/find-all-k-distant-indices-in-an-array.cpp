3a44c87e4f480be3d4a179941b25d19c3737e61b
Abdullah Ayman <102327084+AbdullahAyman02@users.noreply.github.com>
2025-06-24 17:19:11 +0300

    Time: 0 ms (100.00%) | Memory: 14.6 MB (73.39%) - LeetSync

---
+++ LeetCode/2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        vector<int> result;
+        int r = 0;
+        for (int j = 0, length = nums.size(); j < length;  j++) {
+            if (nums[j] == key) {
+                int l = max(r, j - k);
+                r = min(length - 1, j + k) + 1;
+                for (int i = l; i < r; i++)
+                    result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
