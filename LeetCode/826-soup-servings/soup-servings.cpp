528ea8555bbc0bb39f655a9ed7a4993cb6376f52
Abdullah Ayman <102327084+AbdullahAyman02@users.noreply.github.com>
2025-08-08 23:28:53 +0300

    Time: 223 ms (5.22%) | Memory: 77.2 MB (38.04%) - LeetSync

---
+++ LeetCode/826-soup-servings/soup-servings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    unordered_map<int, unordered_map<int, double>> dp;
+    
+    double calculateDP(int i, int j) {
+        return 0.25 * (dp[max(0, i - 4)][j] + dp[max(0, i - 3)][j - 1] + dp[max(0, i - 2)][max(0, j - 2)] + dp[i - 1][max(0, j - 3)]);
+    }
+
+public:
+    double soupServings(int n) {
+        int m = ceil(n / 25.0);
+
+        dp[0][0] = 0.5;
+        for (int k = 1; k <= m; k++) {
+            dp[0][k] = 1;
+            dp[k][0] = 0;
+
+            for (int j = 1; j <= k; j++) {
+                dp[j][k] = calculateDP(j, k);
+                dp[k][j] = calculateDP(k, j);
+            }
+
+            if (dp[k][k] > 1 - 1e-5)
+                return 1;
+        }
+
+        return dp[m][m];
+    }
+};
