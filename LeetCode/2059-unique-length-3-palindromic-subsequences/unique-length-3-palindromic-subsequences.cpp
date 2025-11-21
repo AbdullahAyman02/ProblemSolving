68d805bf565779363860160a3e7481fd86c75906
Abdullah Ayman <102327084+AbdullahAyman02@users.noreply.github.com>
2025-11-21 12:41:40 +0200

    Time: 176 ms (40.23%) | Memory: 15.9 MB (58.20%) - LeetSync

--- LeetCode/2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ LeetCode/2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -2,23 +2,28 @@ class Solution {
 public:
     int countPalindromicSubsequence(string s) {
         unordered_set<char> letters;
-        for(auto c : s)
+        for (char c : s) {
             letters.insert(c);
+        }
+
         int result = 0;
-        for(auto letter : letters)
-        {
-            int i, j, length = s.size();
-            for (i = 0; i < length; i++)
-                if(s[i] == letter)
-                    break;
-            for (j = length - 1; j >= 0; --j)
-                if(s[j] == letter)
-                    break;
+        for (char letter : letters) {
+            int i = -1, j = 0;
+
+            for (int k = 0; k < s.size(); k++) {
+                if (s[k] == letter) {
+                    if (i == -1) i = k;
+                    j = k;
+                }
+            }
+
             unordered_set<char> between;
-            for(int k = i + 1; k < j; k++)
+            for (int k = i + 1; k < j; k++)
                 between.insert(s[k]);
+
             result += between.size();
         }
+
         return result;
     }
 };
