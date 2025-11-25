f0c27e3a848d0a5dc0765f31f77899ac2cb0d49b
Abdullah Ayman <102327084+AbdullahAyman02@users.noreply.github.com>
2025-11-25 11:38:32 +0200

    Time: 0 ms (100.00%) | Memory: 7.9 MB (22.83%) - LeetSync

--- LeetCode/1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
+++ LeetCode/1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int smallestRepunitDivByK(int k) {
         if (!(k % 2) || !(k % 5)) return -1;
-        long long num = 1; int result = 1;
-        while (num % k) {
-            num = (num * 10 + 1) % k;
+        int rem = 1; int result = 1;
+        while (rem % k) {
+            rem = (rem * 10 + 1) % k;
             result++;
             if (result > k) return -1;
         }
