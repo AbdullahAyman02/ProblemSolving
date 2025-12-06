a02e22e6adf44bc57574b0d0a33679971c82b608
Abdullah Ayman <102327084+AbdullahAyman02@users.noreply.github.com>
2025-12-06 20:12:29 +0200

    Time: 264 ms (22.43%) | Memory: 232.3 MB (7.48%) - LeetSync

---
+++ LeetCode/3835-count-partitions-with-max-min-difference-at-most-k/count-partitions-with-max-min-difference-at-most-k.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPartitions(vector<int>& nums, int k) {
+        int length = nums.size();
+        long long mod = 1e9 + 7;
+        vector<long long> dp(length + 1);
+        vector<long long> prefix(length + 1);
+        multiset<int> cnt;
+
+        dp[0] = 1;
+        prefix[0] = 1;
+        for (int i = 0, j = 0; i < length; i++) {
+            cnt.emplace(nums[i]);
+
+            while (j <= i && *cnt.rbegin() - *cnt.begin() > k) {
+                cnt.erase(cnt.find(nums[j]));
+                j++;
+            }
+
+            dp[i + 1] = (prefix[i] - (j > 0 ? prefix[j - 1] : 0) + mod) % mod;
+            prefix[i + 1] = (prefix[i] + dp[i + 1]) % mod;
+        }
+
+        return dp[length];
+    }
+};
