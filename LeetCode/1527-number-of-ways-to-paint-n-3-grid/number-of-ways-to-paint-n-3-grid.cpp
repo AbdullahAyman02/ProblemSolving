45ecab533e6b463d24223a2d6dd5c58f3afdcc4e
Abdullah Ayman <102327084+AbdullahAyman02@users.noreply.github.com>
2026-01-03 15:07:46 +0200

    Time: 258 ms (15.67%) | Memory: 92 MB (8.67%) - LeetSync

---
+++ LeetCode/1527-number-of-ways-to-paint-n-3-grid/number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numOfWays(int n) {
+        const int MOD = 1e9 + 7;
+        vector<vector<vector<vector<int>>>> dp(n, vector<vector<vector<int>>>(4, vector<vector<int>>(4, vector<int>(4, -1))));
+        return solve(0, n, 0, 0, 0, MOD, dp);
+    }
+
+    int solve(int i, int n, int prev1, int prev2, int prev3, int MOD, vector<vector<vector<vector<int>>>>& dp) {
+        if (i == n) return 1;
+        if (dp[i][prev1][prev2][prev3] != -1) return dp[i][prev1][prev2][prev3];
+
+        int result = 0;
+        for (int col1 = 1; col1 <= 3; col1++) {
+            if (col1 == prev1) continue;
+            for (int col2 = 1; col2 <= 3; col2++) {
+                if (col2 == prev2 || col2 == col1) continue;
+                for (int col3 = 1; col3 <= 3; col3++) {
+                    if (col3 == prev3 || col3 == col2) continue;
+                    result = (result + solve(i + 1, n, col1, col2, col3, MOD, dp)) % MOD;
+                }
+            }
+        }
+
+        return dp[i][prev1][prev2][prev3] = result;
+    }
+};
