47c4ff8a10f9156386067816791d7bbd90e29e98
Abdullah Ayman <102327084+AbdullahAyman02@users.noreply.github.com>
2026-03-14 22:30:35 +0200

    Time: 0 ms (100.00%) | Memory: 8 MB (80.45%) - LeetSync

--- LeetCode/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ LeetCode/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     string getHappyString(int n, int k) {
-        int total = 3 * (1 << (n - 1));
+        int sz = pow(2, n - 1);
+        if (3 * sz < k) return "";
 
-        if (k > total) return "";
-
-        string result(n, 'a');
-
-        unordered_map<char, char> nextSmallest = { {'a', 'b'}, {'b', 'a'}, {'c', 'a'}};
-        unordered_map<char, char> nextGreatest = { {'a', 'c'}, {'b', 'c'}, {'c', 'b'}};
-
-        int startA = 1;
-        int startB = startA + (1 << (n - 1));
-        int startC = startB + (1 << (n - 1));
-
-        if (k < startB) {
-            result[0] = 'a';
-            k -= startA;
-        } else if (k < startC) {
-            result[0] = 'b';
-            k -= startB;
-        } else {
-            result[0] = 'c';
-            k -= startC;
+        string result = "";
+        if (k <= sz) result += 'a';
+        else if (k <= 2 * sz) {
+            result += 'b';
+            k -= sz;
+        }
+        else {
+            result += 'c';
+            k -= 2 * sz;
         }
 
-        for (int charIndex = 1; charIndex < n; charIndex++) {
-            int midpoint = (1 << (n - charIndex - 1));
-            if (k < midpoint) {
-                result[charIndex] = nextSmallest[result[charIndex - 1]];
-            } else {
-                result[charIndex] = nextGreatest[result[charIndex - 1]];
-                k -= midpoint;
+        string opts[] = { "bc", "ac", "ab" };
+        for (int i = 1; i < n; i++) {
+            sz /= 2;
+            string& ch = opts[result.back() - 'a'];
+            if (k <= sz) result += ch[0];
+            else {
+                result += ch[1];
+                k -= sz;
             }
         }
 
